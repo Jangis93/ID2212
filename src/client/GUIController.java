1437047a58b3f782a4b781992550f9d9f70022c9
unknown <michaela.jangefalk@gmail.com>
2019-03-05 00:43:47 +0100

    implement switch frame method

--- src/client/GUIController.java
+++ src/client/GUIController.java
@@ -5,6 +5,9 @@
  */
 package client;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Michaela
@@ -18,14 +21,38 @@ public class GUIController {
     
     GUIController(){
         mPage = new MainPage();
+        startNewFrame(mPage, "Nordic Olympic Games");
         pPage = new PartPage();
         sPage = new StatsPage();
         connected = true;
     }
     
+
+    
     private void inputListener(){
         
         
+    }
+    
+    private void startNewFrame(JPanel ob, String title){
+        JFrame frame = new JFrame(title);
+        frame.setContentPane(ob);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650, 600);
+        frame.setVisible(true);
+    }
+    
+    
+    public MainPage getMpage(){
+        return this.mPage;
+    }
+    
+    public PartPage getPpage(){
+        return this.pPage;
+    }
+        
+    public StatsPage getSpage(){
+        return this.sPage;
     }
     
 }
